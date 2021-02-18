bcc6125b5306e5dcfe798ae7f8cdd1ac1eca8e1a
Rebraxin <cedric.paje@gmail.com>
2021-02-18 22:45:18 +0100

    [DONE]: - HomeScreen added 	- Product component created 	- Ratin component created

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,27 +1,98 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { fade, makeStyles } from '@material-ui/core'
-import Menu from '@material-ui/core/Menu'
-import Badge from '@material-ui/core/Badge'
-import Slide from '@material-ui/core/Slide'
+import { fade, makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
-import MenuIcon from '@material-ui/icons/Menu'
-import MailIcon from '@material-ui/icons/Mail'
 import Toolbar from '@material-ui/core/Toolbar'
-import MenuItem from '@material-ui/core/MenuItem'
-import MoreIcon from '@material-ui/icons/MoreVert'
-import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
-import CssBaseline from '@material-ui/core/CssBaseline'
+import Typography from '@material-ui/core/Typography'
+import Badge from '@material-ui/core/Badge'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
+import MenuIcon from '@material-ui/icons/Menu'
 import AccountCircle from '@material-ui/icons/AccountCircle'
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import MailIcon from '@material-ui/icons/Mail'
 import NotificationsIcon from '@material-ui/icons/Notifications'
+import MoreIcon from '@material-ui/icons/MoreVert'
 import useScrollTrigger from '@material-ui/core/useScrollTrigger'
+import Slide from '@material-ui/core/Slide'
+import { CssBaseline } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}))
+
+function HideOnScroll(props) {
+  const { children } = props
+  const trigger = useScrollTrigger()
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  )
+}
 
 const Header = (props) => {
   const classes = useStyles()
-  const { children } = props
-
   const [anchorEl, setAnchorEl] = React.useState(null)
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null)
 
@@ -88,14 +159,6 @@ const Header = (props) => {
         </IconButton>
         <p>Notifications</p>
       </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="show 6 cart items" color="inherit">
-          <Badge badgeContent={6} color="secondary">
-            <ShoppingCartIcon />
-          </Badge>
-        </IconButton>
-        <p>Cart</p>
-      </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
         <IconButton
           aria-label="account of current user"
@@ -109,22 +172,25 @@ const Header = (props) => {
       </MenuItem>
     </Menu>
   )
+
   return (
-    <header>
-      <CssBaseline />
-      <HideOnScroll {...props}>
-        <>
-          <AppBar>
+    <>
+      <div className={classes.grow}>
+        <CssBaseline />
+        <HideOnScroll {...props}>
+          <AppBar position="static">
             <Toolbar>
               <IconButton
                 edge="start"
                 className={classes.menuButton}
                 color="inherit"
-                aria-label="menu"
+                aria-label="open drawer"
               >
                 <MenuIcon />
               </IconButton>
-              <Typography variant="h6">e-Shopping</Typography>
+              <Typography className={classes.title} variant="h6" noWrap>
+                e-Shopping
+              </Typography>
               <div className={classes.grow} />
               <div className={classes.sectionDesktop}>
                 <IconButton aria-label="show 4 new mails" color="inherit">
@@ -138,11 +204,6 @@ const Header = (props) => {
                 >
                   <Badge badgeContent={17} color="secondary">
                     <NotificationsIcon />
-                  </Badge>
-                </IconButton>
-                <IconButton aria-label="show 6 cart items" color="inherit">
-                  <Badge badgeContent={6} color="secondary">
-                    <ShoppingCartIcon />
                   </Badge>
                 </IconButton>
                 <IconButton
@@ -169,96 +230,13 @@ const Header = (props) => {
               </div>
             </Toolbar>
           </AppBar>
-          {renderMobileMenu}
-          {renderMenu}
-        </>
-      </HideOnScroll>
+        </HideOnScroll>
+        {renderMobileMenu}
+        {renderMenu}
+      </div>
       <Toolbar />
-      {children}
-    </header>
+    </>
   )
 }
 
 export default Header
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  title: {
-    display: 'none',
-    [theme.breakpoints.up('sm')]: {
-      display: 'block',
-    },
-  },
-  search: {
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    '&:hover': {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(3),
-      width: 'auto',
-    },
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  inputRoot: {
-    color: 'inherit',
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
-    },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
-    },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-}))
-
-function HideOnScroll(props) {
-  const { children, window } = props
-  const trigger = useScrollTrigger({ target: window })
-
-  return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
-  )
-}
-
-HideOnScroll.propTypes = {
-  children: PropTypes.element.isRequired,
-  window: PropTypes.func,
-}
