4a42db51897d5e3cfb1379491857b5059309e704
Rebraxin <cedric.paje@gmail.com>
2021-02-25 21:10:01 +0100

    Added register user and form validation with formik

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,9 @@
 import { USER_LOGIN_REQUEST } from '../constants/userConstants'
 import { USER_LOGIN_SUCCESS } from '../constants/userConstants'
 import { USER_LOGIN_FAIL } from '../constants/userConstants'
+import { USER_REGISTER_REQUEST } from '../constants/userConstants'
+import { USER_REGISTER_SUCCESS } from '../constants/userConstants'
+import { USER_REGISTER_FAIL } from '../constants/userConstants'
 import { USER_LOGOUT } from '../constants/userConstants'
 
 export const userLoginReducer = (state = {}, action) => {
@@ -12,6 +15,19 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload }
     case USER_LOGOUT:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true }
+    case USER_REGISTER_SUCCESS:
+      return { loading: false, userInfo: action.payload }
+    case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload }
     default:
       return state
